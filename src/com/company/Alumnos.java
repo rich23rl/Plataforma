61053e3b60473d81907684afa23b7b44ce7b17e3
rich23rl <52242920+rich23rl@users.noreply.github.com>
2019-10-14 16:25:20 -0600

    avances

--- src/com/company/Alumnos.java
+++ src/com/company/Alumnos.java
@@ -1,4 +1,22 @@
 package com.company;
 
 public class Alumnos {
+    String Nombre;
+    String Apellido;
+    String Intereses;
+    String Descripcion;
+
+    public Alumnos(String Nombre,String Apellido,String Intereses,String Descripcion)
+    {
+        this.Nombre = Nombre;
+        this.Apellido= Apellido;
+        this.Intereses = Intereses;
+        this.Descripcion = Descripcion;
+
+    }
+
+    public void setNome(String nombre){
+        this.Nombre = nombre;
+
+    }
 }
