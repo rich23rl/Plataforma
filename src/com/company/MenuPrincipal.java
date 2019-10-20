547746171cbe30467ff35da6d5296388c62cde57
rich23rl <52242920+rich23rl@users.noreply.github.com>
2019-10-20 15:28:47 -0600

    avances

--- src/com/company/MenuPrincipal.java
+++ src/com/company/MenuPrincipal.java
@@ -1,4 +1,54 @@
 package com.company;
 
+import com.company.AlumnosController;
+import com.company.AlumnosView;
+import com.company.ClaseController;
+import com.company.ClaseView;
+import com.company.LectorDeTeclado;
+import com.company.MaestrosController;
+import com.company.MaestrosView;
+
 public class MenuPrincipal {
+    int opcion;
+    LectorDeTeclado LDT = new LectorDeTeclado();
+    public void MenuPrincipal(){
+        System.out.println("\tPLataforma Virtual");
+        System.out.println("1.Menu de Alumnos");
+        System.out.println("2.Menu de Maestros");
+        System.out.println("3.Menu de Clase");
+    }
+    public int Leeropcion() {
+        opcion = LDT.getInteger("Ingrese una opción", "Intentelo de Nuevo");
+        return opcion;
+    }
+    public int ObtenerRedultadoP(int opcion) {
+        switch (opcion) {
+            case 1: {
+                AlumnosView av = new AlumnosView();
+                AlumnosController ac = new AlumnosController();
+                av.MenuAlumnos();
+                System.out.println();
+                ac.ObtenerResultado(ac.Leeropcion());
+            }
+            break;
+            case 2: {
+                MaestrosView mv = new MaestrosView();
+                MaestrosController mc = new MaestrosController();
+                mv.MenuMaestros();
+                System.out.println();
+                mc.ObtenerResultado(mc.Leeropcion());
+            }
+            break;
+            case 3: {
+                ClaseView cv = new ClaseView();
+                ClaseController cc = new ClaseController();
+                cv.MenuClase();
+                System.out.println();
+                cc.ObtenerResultado(cc.Leeropcion());
+            }
+            break;
+        }
+        return opcion;
+    }
+
 }
