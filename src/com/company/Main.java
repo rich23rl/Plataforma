b2e96e05ceca9c393cbcfe992e4f9537eced196c
rich23rl <52242920+rich23rl@users.noreply.github.com>
2019-10-20 15:12:58 -0600

    avances

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,5 @@ import com.company.alumnos.AlumnosModel;
 import com.company.alumnos.AlumnosView;
 
 public class Main {
-
-    public static void main(String[] args) {
-        AlumnosView av = new AlumnosView();
-        AlumnosController ac = new AlumnosController();
-        av.MenuAlumnos();
-        ac.ObtenerResultado(ac.Leeropcion());
-        ac.presentarListaAlumnos();
-    }
+    
 }
