289588fb9097253a642a7faf954c102cd6c41a41
rich23rl <52242920+rich23rl@users.noreply.github.com>
2019-10-20 15:27:47 -0600

    avances

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,17 @@
 package com.company;
 
-import com.company.AlumnosController;
-import com.company.AlumnosModel;
-import com.company.AlumnosView;
+import com.company.alumnos.AlumnosController;
+import com.company.alumnos.AlumnosModel;
+import com.company.alumnos.AlumnosView;
+
+import java.util.Scanner;
 
 public class Main {
 
+    public static void main(String[] args) {
+        MenuPrincipal mp = new MenuPrincipal();
+        mp.MenuPrincipal();
+        System.out.println();
+        mp.ObtenerRedultadoP(mp.Leeropcion());
+    }
 }
